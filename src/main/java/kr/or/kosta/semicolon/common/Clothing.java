67932ee4cc1546f2b86b7926b3d517731c37b548
박주연 <legm0117@naver.com>
2017-11-22 16:03:15 +0900

    clothing 업로드

--- src/main/java/kr/or/kosta/semicolon/common/Clothing.java
+++ src/main/java/kr/or/kosta/semicolon/common/Clothing.java
@@ -5,7 +5,7 @@ package kr.or.kosta.semicolon.common;
  * @filename         Clothing.java
  * @author           박주연
  * @since            2017. 11. 21.
- * @see				Tops,Bottoms 가 상속받는 클래스
+ * @see				 Tops,Bottoms 가 상속받는 클래스
  *
  * == Modification Infomation (수정 이력) ==
  * 
