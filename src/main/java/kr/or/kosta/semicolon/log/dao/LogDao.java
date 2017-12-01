6f3d2dc72e100f62316310d7466b8e9a4ea66a6f
이재현 <jaehyun@localhost>
2017-12-01 12:10:21 +0900

    - Log, 한글 Sepor 오류 픽스  - index Recommand, popular 추가

--- src/main/java/kr/or/kosta/semicolon/log/dao/LogDao.java
+++ src/main/java/kr/or/kosta/semicolon/log/dao/LogDao.java
@@ -1,8 +1,15 @@
 package kr.or.kosta.semicolon.log.dao;
 
+import java.util.List;
+
+import kr.or.kosta.semicolon.gpurchase.domain.GpurchaseInfo;
 import kr.or.kosta.semicolon.log.domain.Log;
 
 public interface LogDao {
 
 	public int insert(Log log);
+	
+	public List<GpurchaseInfo> mostCategory(int memberNo);
+	
+	public List<GpurchaseInfo> popularPurchase();
 }
--- src/main/java/kr/or/kosta/semicolon/log/service/LogService.java
+++ src/main/java/kr/or/kosta/semicolon/log/service/LogService.java
@@ -1,8 +1,15 @@
 package kr.or.kosta.semicolon.log.service;
 
+import java.util.List;
+
+import kr.or.kosta.semicolon.gpurchase.domain.GpurchaseInfo;
 import kr.or.kosta.semicolon.log.domain.Log;
 
 public interface LogService {
 
 	public int insert(Log log);
+	
+	public List<GpurchaseInfo> mostCategory(int memberNo);
+	
+	public List<GpurchaseInfo> popularPurchase();
 }
